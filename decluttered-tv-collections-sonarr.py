a7bc4bf9638b8cc9d7587f8dc970679c5c1dbc33
Jusremo <53991288+Jusremo@users.noreply.github.com>
2023-12-24 12:06:27 -0700

    Updating requirements.

--- decluttered-tv-collections-sonarr.py
+++ decluttered-tv-collections-sonarr.py
@@ -1,13 +1,5 @@
-import asyncio
-from sys import exit
-from configparser import ConfigParser
 import os.path
-from plexapi.server import PlexServer
-import plexapi.library
-
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
 from subprocess import Popen, PIPE
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 Popen(['python.exe', '-i', dir_path + '\decluttered-tv-collection-sonarr-delayer.py'], stdout=PIPE, stderr=PIPE)
